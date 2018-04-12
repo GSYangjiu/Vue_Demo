d7c835cb7120764da0b44478b2b789168a2c4338
Yangyangyang <1094259423@qq.com>
2018-04-12 17:23:43 +0800

    修改缩进样式

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
-  <div id="app" about="fillcontain">
-    <router-view></router-view>
-  </div>
+    <div id="app" about="fillcontain">
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
-  export default {}
+    export default {}
 </script>
 
 <style lang="less">
-  @import './style/common';
+    @import './style/common';
 </style>
--- src/page/login.vue
+++ src/page/login.vue
@@ -1,55 +1,55 @@
 <template>
-  <div class="login_page fillcontain">
-    <transition name="form-data" mode="in-out">
-      <section class="form_contianer" v-show="showLogin">
-        <div class="manage_tip">
-          <p>ele后台管理系统</p>
-        </div>
-        <el-form :model="loginForm" :rules="rules" ref="loginForm">
-          <el-form-item prop="username">
-            <el-input v-model="loginForm.username" placeholder="用户名">
-              <span>yang</span>
-            </el-input>
-          </el-form-item>
-          <el-form-item prop="password">
-            <el-input v-model="loginForm.password" placeholder="密码">
-              <span>admin</span>
-            </el-input>
-          </el-form-item>
-          <el-form-item>
-            <el-button type="primary" @click="submitForm('loginForm')" class="submit_btn">登陆</el-button>
-          </el-form-item>
-        </el-form>
-      </section>
-    </transition>
-  </div>
+    <div class="login_page fillcontain">
+        <transition name="form-data" mode="in-out">
+            <section class="form_contianer" v-show="showLogin">
+                <div class="manage_tip">
+                    <p>ele后台管理系统</p>
+                </div>
+                <el-form :model="loginForm" :rules="rules" ref="loginForm">
+                    <el-form-item prop="username">
+                        <el-input v-model="loginForm.username" placeholder="用户名">
+                            <span>yang</span>
+                        </el-input>
+                    </el-form-item>
+                    <el-form-item prop="password">
+                        <el-input v-model="loginForm.password" placeholder="密码">
+                            <span>admin</span>
+                        </el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary" @click="submitForm('loginForm')" class="submit_btn">登陆</el-button>
+                    </el-form-item>
+                </el-form>
+            </section>
+        </transition>
+    </div>
 </template>
 
 <script>
-  export default {
-    name: "login",
-    data() {
-      return {
-        loginForm: {
-          username: '',
-          password: ''
+    export default {
+        name: "login",
+        data() {
+            return {
+                loginForm: {
+                    username: '',
+                    password: ''
+                },
+                rules: {
+                    username: [
+                        {required: true, message: '请输入用户名', trigger: 'blur'}
+                    ],
+                    password: [
+                        {required: true, message: '请输入密码', trigger: 'blur'}
+                    ]
+                }
+            }
         },
-        rules: {
-          username: [
-            {required: true, message: '请输入用户名', trigger: 'blur'}
-          ],
-          password: [
-            {required: true, message: '请输入密码', trigger: 'blur'}
-          ]
-        }
-      }
-    },
-    methods: {
-      showLogin: function () {
+        methods: {
+            showLogin: function () {
 
-      }
+            }
+        }
     }
-  }
 </script>
 
 <style scoped>
